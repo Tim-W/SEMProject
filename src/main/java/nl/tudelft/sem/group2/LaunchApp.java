b547bc7a4d5cdad3884a8b84b4c4af8dbe1e3088
dmouwen <dennismouwen@gmail.com>
2016-09-08 11:39:02 +0200

    added canvas support example and scene

--- src/main/java/nl/tudelft/sem/group2/LaunchApp.java
+++ src/main/java/nl/tudelft/sem/group2/LaunchApp.java
@@ -1,29 +1,52 @@
 package main.java.nl.tudelft.sem.group2;
 
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 public class LaunchApp extends Application{
 
-	public static Stage mainStage;
+	public static Stage stage;
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 	
 		
 		// Init variables
-		mainStage = primaryStage;
+		stage = primaryStage;
 
 		// Start stage
-		mainStage.setTitle("Qix");
-		mainStage.show();
-		mainStage.setResizable(false);
-		mainStage.setWidth(340);
-		mainStage.setHeight(400);
-		mainStage.getIcons().add(new Image("http://www.arcadeboss.com/games/qix/qix.gif"));
+		stage.setTitle("Qix");
+		stage.setResizable(false);
+		stage.setWidth(340);
+		stage.setHeight(400);
+		stage.getIcons().add(new Image("http://www.arcadeboss.com/games/qix/qix.gif"));
+		
+		Scene scene;
+		Group root = new Group();
+		
+		scene = new Scene(root,340,400, Color.BLACK);
+
+		//EXAMPLE ON HOW TO USE CANVAS
+//		final Canvas canvas = new Canvas(300,300);	
+//		GraphicsContext gc = canvas.getGraphicsContext2D();
+//		 
+//		gc.setFill(Color.BLUE);
+//		gc.fillRect(75,75,100,100);
+//		root.getChildren().add(canvas);
+		
+
+		stage.setScene(scene);
+		stage.show();
 
 	}
 
