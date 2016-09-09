27894f3bcedb915a1487da1c68faa76d21cb82be
Dennis Mouwen <dennismouwen@gmail.com>
2016-09-09 11:08:23 +0200

    removed wrong launcher, renamed file to right syntax

--- src/main/java/nl/tudelft/sem/group2/scenes/scoreScene.java
+++ src/main/java/nl/tudelft/sem/group2/scenes/scoreScene.java
@@ -5,12 +5,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.text.Font;
 
-public class scoreScene extends Scene{
+public class ScoreScene extends Scene{
 
 	
 
 	
-	public scoreScene(Parent arg0, double arg1, double arg2, boolean arg3) {
+	public ScoreScene(Parent arg0, double arg1, double arg2, boolean arg3) {
 		super(arg0, arg1, arg2, arg3);
 		// TODO Auto-generated constructor stub
 		
