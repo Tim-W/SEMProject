c849010d79a348789423ca21368bbbc880b5c253
dmouwen <dennismouwen@gmail.com>
2016-09-09 17:44:07 +0200

    started on new gui for scorescene and gamescene, edited launchapp to use gamescene

--- src/main/java/nl/tudelft/sem/group2/scenes/ScoreScene.java
+++ src/main/java/nl/tudelft/sem/group2/scenes/ScoreScene.java
@@ -1,22 +1,97 @@
 package main.java.nl.tudelft.sem.group2.scenes;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.SubScene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import main.java.nl.tudelft.sem.group2.LaunchApp;
 
-public class ScoreScene extends Scene{
+public class ScoreScene extends SubScene{
 
+	private Label score;
+	private Label claimedPercentage;
+	private Label title;
+	private int targetPercentage;
+	private Label claimed = new Label("Claimed");
 	
 
 	
-	public ScoreScene(Parent arg0, double arg1, double arg2, boolean arg3) {
-		super(arg0, arg1, arg2, arg3);
-		// TODO Auto-generated constructor stub
+	public ScoreScene(Group stage, double width, double height){
+		super(stage, width, height);
+		targetPercentage = 75;
+		claimed.setTextFill(Color.YELLOW);
+		claimed.setStyle("-fx-font-size:14;");
 		
-		Font.loadFont(LaunchApp.class.getResource("QixFont.ttf").toExternalForm(),10);
+		TilePane tilePane = new TilePane();
+		tilePane.prefTileWidthProperty().setValue(113);
+		tilePane.setPadding(new Insets(20));
+		tilePane.setVgap(10);
+		
+		VBox left = new VBox();
+		VBox center = new VBox();
+		center.setSpacing(-5);
+		VBox right = new VBox();
+		right.setAlignment(Pos.CENTER_LEFT);
+		
+		score = new Label();
+		score.setTextFill(Color.WHITE);
+		score.setStyle("-fx-font-size:24;");
 
+		
+		claimedPercentage = new Label();
+		claimedPercentage.setTextFill(Color.YELLOW);
+		claimedPercentage.setStyle("-fx-font-size:14;");
+
+		
+		title = new Label("Qix");
+		title.setTextFill(Color.YELLOW);
+		title.setStyle("-fx-font-size:30;");
+
+		//TODO Fix font
+		//Font f = Font.loadFont(LaunchApp.class.getResource("qixfont.ttf").toExternalForm(),12);
+		//title.setFont(f);
+
+		left.getChildren().add(title);
+		center.getChildren().add(claimed);
+		center.getChildren().add(claimedPercentage);
+		right.getChildren().add(score);
+		
+		tilePane.getChildren().add(left);
+		tilePane.getChildren().add(center);
+		tilePane.getChildren().add(right);
+
+		
+		stage.getChildren().add(tilePane);
 	}
+
+
+
+
+	public void setScore(int scoreInput) {
+		score.setText(String.valueOf(scoreInput));
+	}
+
+
+
+
+	public void setClaimedPercentage(int claimedPercentageInput) {
+		claimedPercentage.setText(String.valueOf(claimedPercentageInput) + "% of " + String.valueOf(targetPercentage) + "%");
+	}
+	
+	
 
 	
 	
