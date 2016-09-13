d2756db4c93e27432defca8ddd7e157c297d7597
Dennis Mouwen <dennismouwen@gmail.com>
2016-09-13 16:38:16 +0200

    major acceleration improvement

--- src/main/java/nl/tudelft/sem/group2/game/Board.java
+++ src/main/java/nl/tudelft/sem/group2/game/Board.java
@@ -9,63 +9,66 @@ import nl.tudelft.sem.group2.units.Qix;
 import nl.tudelft.sem.group2.units.Sparx;
 import nl.tudelft.sem.group2.units.Unit;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Board {
-    private Canvas canvas;
-    private Set<Unit> units;
-    private GraphicsContext gc;
+	private Canvas canvas;
+	private Set<Unit> units;
+	private GraphicsContext gc;
 
-    public Board(Canvas canvas) {
-        this.units = new HashSet<Unit>();
-        this.canvas = canvas;
-        gc = canvas.getGraphicsContext2D();
-        //BLUE SCREEN IS THE SIZE OF THE BOARD, 300x300
-        gc.setFill(Color.BLUE);
-        gc.fillRect(0, 0, 300, 300);
-    }
+	public Board(Canvas canvas) {
+		this.units = new HashSet<Unit>();
+		this.canvas = canvas;
+		gc = canvas.getGraphicsContext2D();
+		// BLUE SCREEN IS THE SIZE OF THE BOARD, 300x300
+		gc.setFill(Color.BLUE);
+		gc.fillRect(0, 0, 300, 300);
+	}
 
-    public void setUnits(Set<Unit> units) {
-        this.units = units;
-    }
+	public void setUnits(Set<Unit> units) {
+		this.units = units;
+	}
 
-    public Set<Unit> getUnits() {
-        return units;
-    }
+	public Set<Unit> getUnits() {
+		return units;
+	}
 
-    public void addUnit(Unit unit) {
-        this.units.add(unit);
-    }
+	public void addUnit(Unit unit) {
+		this.units.add(unit);
+	}
 
-    public boolean removeUnit(Unit unit) {
-        return this.units.remove(unit);
-    }
+	public boolean removeUnit(Unit unit) {
+		return this.units.remove(unit);
+	}
 
-    public void draw() {
-        gc.fillRect(0, 0, 310, 310);
-        for (Unit unit : units) {
-            unit.move();
-            unit.draw(canvas);
-        }
+	public void draw() {
+		gc.fillRect(0, 0, 310, 310);
+		for (Unit unit : units) {
+			unit.move();
+			unit.draw(canvas);
+		}
 
-    }
-    
-    public void collisions(){
-    	for (Unit collider: units) {
-    		for(Unit collidee : units){
-    			if(collider instanceof Qix || collidee instanceof Qix){
-    				//TODO code for qix collisions/intersect
-    			}
-    			else if(collider instanceof Cursor && collider != collidee){
-    				if((collidee instanceof Sparx || collidee instanceof Fuse) &&collider.intersect(collidee)){
-    					System.out.println("dood");
-    					//TODO code to start game over sequence
-    				}
-    			}
-    		}
-    	}
-    }
-    
+	}
+
+	public void collisions() {
+		ArrayList<Unit> unitsList = new ArrayList<Unit>();
+		unitsList.addAll(units);
+		for (int i = 0; i < unitsList.size(); i++) {
+			Unit collider = unitsList.get(i);
+			unitsList.remove(i);
+			for (Unit collidee : unitsList) {
+				if (collider instanceof Qix || collidee instanceof Qix) {
+					// TODO code for qix collisions/intersect
+				} else if (collider instanceof Cursor) {
+					if ((collidee instanceof Sparx || collidee instanceof Fuse) && collider.intersect(collidee)) {
+						System.out.println("dood");
+						// TODO code to start game over sequence
+					}
+				}
+			}
+		}
+	}
 
 }
