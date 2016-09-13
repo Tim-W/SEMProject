a027d42ae72499061b63a1338aed8b54b35fb147
Gijs Koning <g.koning@student.tudelft.nl>
2016-09-13 12:15:49 +0200

    add areastate

---
+++ src/main/java/nl/tudelft/sem/group2/AreaState.java
@@ -0,0 +1,11 @@
+package nl.tudelft.sem.group2;
+
+/**
+ * Created by gijs on 13-9-2016.
+ */
+/**
+ * Enum for all the posible states of a pixel on the board
+ */
+public enum AreaState {
+    UNCOVERED, FAST, SLOW, BORDER
+}
