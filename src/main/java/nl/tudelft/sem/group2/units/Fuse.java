7a86a90597bb9cd4ce695bd1a43b641b5f2b0930
Tim Wissel <twtimwissel5@gmail.com>
2016-09-14 18:54:43 +0200

    Create 2 sparx, create fuse

--- src/main/java/nl/tudelft/sem/group2/units/Fuse.java
+++ src/main/java/nl/tudelft/sem/group2/units/Fuse.java
@@ -1,14 +1,46 @@
 package nl.tudelft.sem.group2.units;
 
+import javafx.scene.image.*;
+
+import java.awt.*;
+import java.awt.Image;
+
 public class Fuse extends LineTraveller {
+    private int speed = 1;
+    private int lastX = 75;
+    private int lastY = 150;
+
     public Fuse(int x, int y, int width, int height) {
         super(x, y, width, height);
+        this.sprite = new javafx.scene.image.Image[2];
+        this.sprite[0] = new javafx.scene.image.Image("/images/supersparx-1.png");
+        this.sprite[1] = new javafx.scene.image.Image("/images/supersparx-2.png");
     }
 
     @Override
     public void move(){
+        for (int i = 0; i < speed; i++) {
+            if (x < 150 && !(x + 1 == lastX) && areaTracker.getStix().contains(new Point(x + 1, y))) {
+                setLastCoordinates(x, y);
+                setX(x + 1);
+            } else if (y < 150 && !(lastY == y + 1) && areaTracker.getStix().contains(new Point(x, y + 1))) {
+                setLastCoordinates(x, y);
+                setY(y + 1);
+            } else if (x > 0 && !(lastX == x - 1) && areaTracker.getStix().contains(new Point(x - 1, y))) {
+                setLastCoordinates(x, y);
+                setX(x - 1);
+            } else if (y > 0 && !(lastY == y - 1) && areaTracker.getStix().contains(new Point(x, y - 1))) {
+                setLastCoordinates(x, y);
+                setY(y - 1);
+            }
+        }
     }
-    
+
+    private void setLastCoordinates(int x, int y) {
+        this.lastX = x;
+        this.lastY = y;
+    }
+
     public String toString() {
     	return "Fuse";
     }
--- src/main/java/nl/tudelft/sem/group2/units/Sparx.java
+++ src/main/java/nl/tudelft/sem/group2/units/Sparx.java
@@ -5,34 +5,59 @@ import javafx.scene.image.Image;
 import java.awt.*;
 
 public class Sparx extends LineTraveller {
-    private int speed = 5;
+    private final SparxDirection sparxDirection;
+    private int speed = 2;
     private int lastX = 0;
     private int lastY = 0;
 
-    public Sparx(int x, int y, int width, int height) {
+
+    public Sparx(int x, int y, int width, int height, SparxDirection sparxDirection) {
         super(x, y, width, height);
         this.sprite = new Image[2];
         this.sprite[0] = new Image("/images/sparx-1.png");
         this.sprite[1] = new Image("/images/sparx-2.png");
+        this.sparxDirection = sparxDirection;
     }
 
     @Override
     public void move() {
-        for (int i = 0; i < speed; i++) {
-            if (x < 150 && !(x + 1 == lastX) && outerBorderOn(x + 1, y)) {
-                setLastCoordinates(x, y);
-                setX(x + 1);
-            } else if (y < 150 && !(lastY == y + 1) && outerBorderOn(x, y + 1)) {
-                setLastCoordinates(x, y);
-                setY(y + 1);
-            } else if (x > 0 && !(lastX == x - 1) && outerBorderOn(x - 1, y)) {
-                setLastCoordinates(x, y);
-                setX(x - 1);
-            } else if (y > 0 && !(lastY == y - 1) && outerBorderOn(x, y - 1)) {
-                setLastCoordinates(x, y);
-                setY(y - 1);
-            }
+        switch (sparxDirection) {
+            case LEFT:
+                for (int i = 0; i < speed; i++) {
+                    if (x > 0 && !(lastX == x - 1) && outerBorderOn(x - 1, y)) {
+                        setLastCoordinates(x, y);
+                        setX(x - 1);
+                    } else if (y > 0 && !(lastY == y - 1) && outerBorderOn(x, y - 1)) {
+                        setLastCoordinates(x, y);
+                        setY(y - 1);
+                    } else if (x < 150 && !(x + 1 == lastX) && outerBorderOn(x + 1, y)) {
+                        setLastCoordinates(x, y);
+                        setX(x + 1);
+                    } else if (y < 150 && !(lastY == y + 1) && outerBorderOn(x, y + 1)) {
+                        setLastCoordinates(x, y);
+                        setY(y + 1);
+                    }
+                }
+                break;
+            case RIGHT:
+                for (int i = 0; i < speed; i++) {
+                    if (x < 150 && !(x + 1 == lastX) && outerBorderOn(x + 1, y)) {
+                        setLastCoordinates(x, y);
+                        setX(x + 1);
+                    } else if (y < 150 && !(lastY == y + 1) && outerBorderOn(x, y + 1)) {
+                        setLastCoordinates(x, y);
+                        setY(y + 1);
+                    } else if (x > 0 && !(lastX == x - 1) && outerBorderOn(x - 1, y)) {
+                        setLastCoordinates(x, y);
+                        setX(x - 1);
+                    } else if (y > 0 && !(lastY == y - 1) && outerBorderOn(x, y - 1)) {
+                        setLastCoordinates(x, y);
+                        setY(y - 1);
+                    }
+                }
+                break;
         }
+
     }
 
     private void setLastCoordinates(int x, int y) {
