6aa77ca0a7a6c916f4ed2da5c135fc48cc35b1a4
Gijs Koning <g.koning@student.tudelft.nl>
2016-09-14 13:11:30 +0200

    Fixed start animation

--- src/main/java/nl/tudelft/sem/group2/units/Cursor.java
+++ src/main/java/nl/tudelft/sem/group2/units/Cursor.java
@@ -7,6 +7,7 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 
 import java.awt.*;
+import java.util.LinkedList;
 
 import static nl.tudelft.sem.group2.game.Board.gridToCanvas;
 
@@ -14,6 +15,7 @@ public class Cursor extends LineTraveller {
     private KeyCode currentMove = null;
     private int loops = 0;
     private int speed = 2;
+    private LinkedList<double[][]> oldLines = new LinkedList<double[][]>();
 
     public void setSpeed(int speed) {
         this.speed = speed;
@@ -107,23 +109,44 @@ public class Cursor extends LineTraveller {
             double heightVar = height / animationSpeed * loops;
             double width = canvas.getWidth();
             double widthVar = width / animationSpeed * loops;
-            double lineSize = 40.0;
+            double lineSize = 80.0;
             double lineSizeVar = (lineSize / animationSpeed) * loops;
-            gC.beginPath();
+            double[][] line = new double[4][4];
+            line[0][0]=width - widthVar + drawX - (lineSize - lineSizeVar);
+            line[0][1]=-(height - heightVar) + drawY;
+            line[0][2]=width - widthVar + drawX;
+            line[0][3]=-(height - heightVar) + drawY + (lineSize - lineSizeVar);
+            line[1][0]=width - widthVar + drawX - (lineSize - lineSizeVar);
+            line[1][1]=height - heightVar + drawY;
+            line[1][2]=width - widthVar + drawX;
+            line[1][3]=height - heightVar + drawY - (lineSize - lineSizeVar);
+            line[2][0]=-(width - widthVar) + drawX + (lineSize - lineSizeVar);
+            line[2][1]=-(height - heightVar) + drawY;
+            line[2][2]=-(width - widthVar) + drawX;
+            line[2][3]=-(height - heightVar) + drawY + (lineSize - lineSizeVar);
+            line[3][0]=-(width - widthVar) + drawX + (lineSize - lineSizeVar);
+            line[3][1]=height - heightVar + drawY;
+            line[3][2]=-(width - widthVar) + drawX;
+            line[3][3]=height - heightVar + drawY - (lineSize - lineSizeVar);
+            oldLines.addFirst(line);
+            if(oldLines.size()>10){
+                oldLines.removeLast();
+            }
+
             gC.setStroke(Color.WHITE);
-            //upRightCorner
-            gC.moveTo(width - widthVar + drawX - (lineSize - lineSizeVar), -(height - heightVar) + drawY);
-            gC.lineTo(width - widthVar + drawX, -(height - heightVar) + drawY + (lineSize - lineSizeVar));
-            //downRightCorner
-            gC.moveTo(width - widthVar + drawX - (lineSize - lineSizeVar), height - heightVar + drawY);
-            gC.lineTo(width - widthVar + drawX, height - heightVar + drawY - (lineSize - lineSizeVar));
-            //upLeftCorner
-            gC.moveTo(-(width - widthVar) + drawX + (lineSize - lineSizeVar), -(height - heightVar) + drawY);
-            gC.lineTo(-(width - widthVar) + drawX, -(height - heightVar) + drawY + (lineSize - lineSizeVar));
-            //downLeftCorner
-            gC.moveTo(-(width - widthVar) + drawX + (lineSize - lineSizeVar), height - heightVar + drawY);
-            gC.lineTo(-(width - widthVar) + drawX, height - heightVar + drawY - (lineSize - lineSizeVar));
-            gC.stroke();
+            for (double[][] l :oldLines
+                 ) {
+                gC.beginPath();
+                for (int i = 0; i < 4; i++) {
+                    gC.moveTo(l[i][0],l[i][1] );
+                    gC.lineTo(l[i][2],l[i][3] );
+
+                }
+                gC.stroke();
+            }
+
+
+
             loops++;
         }
         canvas.getGraphicsContext2D().drawImage(sprite[spriteIndex], drawX - width / 2 + 1, drawY - height / 2 + 1, width, height);
