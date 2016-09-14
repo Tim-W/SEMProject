d17530fa12685467db73707fdb40e57f6865a42e
Tim Wissel <twtimwissel5@gmail.com>
2016-09-14 17:15:44 +0200

    Move sparx

--- src/main/java/nl/tudelft/sem/group2/units/Sparx.java
+++ src/main/java/nl/tudelft/sem/group2/units/Sparx.java
@@ -5,7 +5,9 @@ import javafx.scene.image.Image;
 import java.awt.*;
 
 public class Sparx extends LineTraveller {
-    private int speed = 2;
+    private int speed = 10;
+    private int lastX = 0;
+    private int lastY = 0;
 
     public Sparx(int x, int y, int width, int height) {
         super(x, y, width, height);
@@ -17,16 +19,24 @@ public class Sparx extends LineTraveller {
     @Override
     public void move() {
         for (int i = 0; i < speed; i++) {
-            if (outerBorderOn(x + 1, y)) {
+            if (x < 150 && lastX != x + 1 || lastY != y && outerBorderOn(x + 1, y)) {
                 setX(x + 1);
-            } else if (outerBorderOn(x - 1, y)) {
-                setX(x - 1);
-            } else if (outerBorderOn(x, y + 1)) {
+            } else if (y < 150 && lastY != y + 1 || lastX != x && outerBorderOn(x, y + 1)) {
                 setY(y + 1);
-            } else if (outerBorderOn(x, y - 1)) {
+            } else if (x > 0 && lastX != x - 1 || lastY != y && outerBorderOn(x - 1, y)) {
+                setX(x - 1);
+            } else if (y > 0 && lastY != y - 1 || lastX != x && outerBorderOn(x, y - 1)) {
                 setY(y - 1);
             }
+            setX(x);
+            setY(y);
+            setLastCoordinates(x, y);
         }
+    }
+
+    private void setLastCoordinates(int x, int y) {
+        this.lastX = x;
+        this.lastY = y;
     }
 
     public String toString() {
