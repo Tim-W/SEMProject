daf70bd098a6eec3b477de6a4ed8ad648c5832d2
Tim Wissel <twtimwissel5@gmail.com>
2016-09-22 13:16:51 +0200

    Abide checkstyle rules for LaunchApp class

--- src/main/java/nl/tudelft/sem/group2/LaunchApp.java
+++ src/main/java/nl/tudelft/sem/group2/LaunchApp.java
@@ -10,30 +10,55 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import nl.tudelft.sem.group2.scenes.GameScene;
 
+/**
+ * Starts the application.
+ */
 public class LaunchApp extends Application {
 
-    public static Stage stage;
-    public static MediaView mediaView;
-    private static int boardWidth = 300;
-    private static int boardHeight = 300;
+    private static final int BOARD_WIDTH = 300;
+    private static final int BOARD_HEIGHT = 300;
+    private static final int GAME_WIDTH = 340;
+    private static final int GAME_HEIGHT = 420;
+    private static Stage stage;
+    private static MediaView mediaView;
 
+    /**
+     * @return board width
+     */
     public static int getBoardWidth() {
-        return boardWidth;
+        return BOARD_WIDTH;
     }
 
+    /**
+     * @return board height
+     */
     public static int getBoardHeight() {
-        return boardHeight;
+        return BOARD_HEIGHT;
     }
 
-    // a point on the boardgrid is 2x2 pixels, so a boardgrid contains 150x150
+    /**
+     * @return grid height - a point on the boardgrid is 2x2 pixels,
+     * so a boardgrid contains 150x150
+     */
     public static int getGridHeight() {
-        return boardHeight / 2;
+        // a point on the boardgrid is 2x2 pixels, so a boardgrid contains 150x150
+        return BOARD_HEIGHT / 2;
     }
 
+    /**
+     * @return grid width - a point on the boardgrid is 2x2 pixels,
+     * so a boardgrid contains 150x150
+     */
     public static int getGridWidth() {
-        return boardWidth / 2;
+        return BOARD_WIDTH / 2;
     }
 
+    /**
+     * Plays a sound file.
+     *
+     * @param string - the path to the sound file
+     * @param volume - the volume in decibels
+     */
     public static synchronized void playSound(final String string, final double volume) {
         new Thread(new Runnable() {
             // The wrapper thread is unnecessary, unless it blocks on the
@@ -51,7 +76,11 @@ public class LaunchApp extends Application {
             }
         }).start();
     }
-    
+
+    /**
+     * Launches the application.
+     * @param args - not used
+     */
     public static void main(String[] args) {
         launch(args);
     }
@@ -65,8 +94,8 @@ public class LaunchApp extends Application {
 
         // Start stage
         stage.setTitle("Qix");
-        stage.setWidth(340);
-        stage.setHeight(420);
+        stage.setWidth(GAME_WIDTH);
+        stage.setHeight(GAME_HEIGHT);
         stage.getIcons().add(new Image("/images/stageIcon.png"));
 
         GameScene scene;
