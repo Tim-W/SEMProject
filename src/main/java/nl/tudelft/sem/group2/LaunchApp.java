34ceb0f6590cf39b157157d33303e5f808ab6ffd
Dennis Mouwen <dennismouwen@gmail.com>
2016-09-22 14:26:07 +0200

    can now turn logging off

--- src/main/java/nl/tudelft/sem/group2/LaunchApp.java
+++ src/main/java/nl/tudelft/sem/group2/LaunchApp.java
@@ -99,6 +99,8 @@ public class LaunchApp extends Application {
     public static void main(String[] args) {
     	if(args.length > 0 && args[0].equals("detailedLogging")){
     		LOGGER.setLevel(Level.ALL);
+    	} else if (args.length > 0 && args[0].equals("loggingOff")){
+    		LOGGER.setLevel(Level.OFF);
     	}
         launch(args);
     }
