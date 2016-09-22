5d0f5990887b7f288871824e3e85988d3185d57c
Dennis Mouwen <dennismouwen@gmail.com>
2016-09-22 14:11:04 +0200

    added new Logger.java class

---
+++ src/main/java/nl/tudelft/sem/group2/Logger.java
@@ -0,0 +1,25 @@
+package nl.tudelft.sem.group2;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+
+public class Logger {
+
+	private Level level;
+
+	public Logger() {
+		level = Level.INFO;
+	}
+
+	public void log(Level logLevel, String message, Class<?> logClass) {
+		if (level.intValue() <= logLevel.intValue()) {
+			String time = new SimpleDateFormat("HH:mm:ss").format(new Date());
+			System.err.println(time + " " + logClass.getName() + "\n" + message);
+		}
+	}
+
+	public void setLevel(Level level) {
+		this.level = level;
+	}
+}
