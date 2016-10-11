132ed1dd547ebb11d517b6ed2f4b029864e17e88
dennism1997 <D.Mouwen@student.tudelft.nl>
2016-10-11 11:14:08 +0200

    added javadoc and fixed checkstyle

--- src/main/java/nl/tudelft/sem/group2/collisions/CollisionInterface.java
+++ src/main/java/nl/tudelft/sem/group2/collisions/CollisionInterface.java
@@ -4,8 +4,15 @@ import nl.tudelft.sem.group2.units.Unit;
 
 /**
  * Created by Dennis on 11-10-16
+ * Interface for collisions, using the intersect method.
  */
 public interface CollisionInterface {
 
-    public boolean intersect(Unit other);
+    /**
+     * method that checks for intersections between the calling Unit and the parameter unit.
+     *
+     * @param unit the unit collisions need to be checked with
+     * @return true if the units intersect, false if they don't
+     */
+    boolean intersect(Unit unit);
 }
