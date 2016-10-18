7494f8ed8200afd9e04bbb27ecde4737cae0fce7
Gijs Koning <g.koning@student.tudelft.nl>
2016-10-18 09:39:42 +0200

    maybe fix for travis

--- src/main/java/nl/tudelft/sem/group2/LaunchApp.java
+++ src/main/java/nl/tudelft/sem/group2/LaunchApp.java
@@ -9,7 +9,6 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import nl.tudelft.sem.group2.scenes.StartScene;
 
-import java.awt.EventQueue;
 import java.util.logging.Level;
 
 import static nl.tudelft.sem.group2.global.Globals.BOARD_HEIGHT;
@@ -54,12 +53,7 @@ public class LaunchApp extends Application {
         } else if (args.length > 0 && args[0].equals("loggingOff")) {
             LOGGER.setLevel(Level.OFF);
         }
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                javafx.application.Application.launch(LaunchApp.class);
-            }
-        });
+        launch(args);
     }
 
     /**
