a2dc8f86b109cf74352fca961b5084f261f6c1d6
Tim Wissel <twtimwissel5@gmail.com>
2016-10-21 14:41:02 +0200

    Merge feature/switch-case-rewrites

--- src/main/java/nl/tudelft/sem/group2/AreaTracker.java
+++ src/main/java/nl/tudelft/sem/group2/AreaTracker.java
@@ -1,8 +1,12 @@
 package nl.tudelft.sem.group2;
 
 import java.awt.Point;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.Stack;
 import java.util.concurrent.ThreadLocalRandom;
